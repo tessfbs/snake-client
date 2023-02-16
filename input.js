320d18cbe742cae6cd76c4bed05de91c4b449f53
tessfbs <tessfbs@gmail.com>
2023-02-15 22:36:41 -0500

    WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = () => {
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
@@ -12,6 +16,22 @@ const handleUserInput = (command) => {
   if(command === "\u0003" ){
     console.log("detected ctrl + c ")
     process.exit();
+  }
+  if(command === "\u0077" ){
+    console.log("Move: up")
+    connection.write("Move: up");
+    }
+  if(command === "\u0061" ){
+    console.log("Move: left")
+    connection.write("Move: left");
+  }
+  if(command === "\u0073" ){
+    console.log("Move: down")
+    connection.write("Move: down");
+  }
+  if(command === "\u0064" ){
+    console.log("Move: right")
+    connection.write("Move: right");
   }
 };
 
